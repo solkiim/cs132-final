6cb837c0547955507c829fe79630e6bcc36ce7bc
rkim <rhaime.kim@gmail.com>
2018-04-16 00:54:54 -0400

    reorg rkim's files

--- sql/order-tables.sql
+++ sql/order-tables.sql
@@ -7,12 +7,9 @@ CREATE TABLE IF NOT EXISTS Token (
 	UNIQUE (tokenName)
 );
 
-<<<<<<< HEAD
-
 -- want buy and sell to go from HIGHEST ----> to LOWEST price
 
-
-CREATE TABLE Buy (
+CREATE TABLE IF NOT EXISTS Buy (
 
 	orderID				INTEGER AUTO_INCREMENT,
 	tokenSymbol			VARCHAR(5) NOT NULL,
@@ -28,11 +25,24 @@ CREATE TABLE Buy (
 
 );
 
-CREATE TABLE Sell (
+CREATE TABLE IF NOT EXISTS Sell (
 
-=======
+	orderID				INTEGER AUTO_INCREMENT,
+	tokenSymbol			VARCHAR(5) NOT NULL,
+	buyOrSell			TEXT NOT NULL,
+	orderType			VARCHAR(4) NOT NULL,
+	-- either num tokens OR price should be filled
+	numTokens         	INTEGER,
+	price         		INTEGER,
+	username			TEXT,
+	timestamp_			DATETIME DEFAULT NOW() NOT NULL,
+	completed			BOOLEAN DEFAULT 0,
+	PRIMARY KEY (orderID)
+
+);
+
 CREATE TABLE IF NOT EXISTS Order (
->>>>>>> 287b48721251baa6b72a207773ac9b1b52f5b95e
+
 	orderID				INTEGER AUTO_INCREMENT,
 	tokenSymbol			VARCHAR(5) NOT NULL,
 	orderType			VARCHAR(4) NOT NULL,
