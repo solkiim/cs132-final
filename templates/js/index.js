25825be0840c30c21e68d8e5cad47655a23864f3
Sol Kim <solkim813@gmail.com>
2018-04-17 14:37:15 -0400

    extract header logic

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,38 +1,3 @@
 $(document).ready(function() {
-	if (sessionStorage.getItem('username')) {
-		// if user already logged in session
-		$('#account-info').show();
-		$('#username').text(sessionStorage.getItem('username'));
-	} else {
-		$('#login-signup').show();
-	}
 	
-	// sign out clicked
-	$('#sign-out').click(function(event) {
-		sessionStorage.removeItem('username');
-		$('#account-info').hide();
-		$('#login-signup').show();
-	});
-	
-	// login form submitted
-	$('#loginform').submit(function(event) {
-		event.preventDefault();
-		
-		$.post(
-			'/verifypassword',
-			$('#loginform').serialize(),
-			function(data, status) {
-				if (data == 'that username does not exist') {
-					alert(data);
-				} else if (data == true) {
-					// if username and password match
-					sessionStorage.setItem('username', $('#loginform input[name=username]').val());
-					window.location.href = '/account';
-				} else {
-					alert('incorrect username/password combo');
-					$("#loginform")[0].reset();
-				}
-			}
-		);
-	});
 });
