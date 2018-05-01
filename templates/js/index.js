b646f9103a670f931155433d2fd822cf8f291f92
rkim <rhaime.kim@gmail.com>
2018-05-01 16:53:34 -0400

    merged

--- templates/js/index.js
+++ templates/js/index.js
@@ -1,7 +1,7 @@
 // var socket = io.connect();
 
-// const io = require('socket.io-client');
-// const socket = io.connect('http://localhost:8080');
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:8080');
 
 $(document).ready(function() {
 	
