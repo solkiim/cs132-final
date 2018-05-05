3ee4a5b320d1fcd2e31ac090c4172cf149ebaf85
rkim <rhaime.kim@gmail.com>
2018-05-04 21:55:56 -0400

    index.js orders

--- templates/js/index.js
+++ templates/js/index.js
@@ -44,14 +44,15 @@ $(document).ready(function() {
 
 			if (buyOrSell == "sell"){
 				ul = $('#sell-list');
+				ul.append($('<li></li>').html('<div class="sell-time">' + time + '</div><div class="sell-buyorsell">' + buyOrSell + '</div><div class="sell-price">' + price + '</div><div class="sell-numTokens">' + numTokens + '</div>'));
 
 			} else if (buyOrSell == "buy"){
 				ul = $('#buy-list');
-			}
-
-			ul.append($('<li></li>').text(time + ', ' + buyOrSell + ', ' + price + ', ' + numTokens));
+				ul.append($('<li></li>').html('<div class="buy-time">' + time + '</div><div class="buy-buyorsell">' + buyOrSell + '</div><div class="buy-price">' + price + '</div><div class="buy-numTokens">' + numTokens + '</div>'));
 
 			}
+		}
+
 	});
 
 	$('#buyForm').submit(function(event) {
